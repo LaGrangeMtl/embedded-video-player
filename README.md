b0b77d323ef9df5a81b9cae07f04f31756ad7b11
Nicolas Poirier-Barolet <picolo@la-grange.ca>
2017-01-23 14:31:27 -0500

    Added files + Update README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # embedded-video-player
+Construit un player vidéo avec un iFrame, un bouton close et les shares du vidéo. Le player se reconstruit à chaque click sur un élément déclencheur. Il n'y a donc qu'une seule instance de ce player dans une page donnée.
+
+---
+
+## Fonctionnement :
+
+* Importer le fichier SCSS dans votre projet : `@import 'path/to/_video-player;`
+* Importer le JS dans votre projet : `import VideoPlayer from 'path/to/VideoPlayer';`
+* Changer la valeur de `FB_APP_ID` pour l'app ID Facebook du client
+* Initialiser le JS : `VideoPlayer.init();`
+
+Le sélecteur CSS pour les éléments déclencheurs est par défaut `[data-video-btn]`.
+ 
+On met le ID Youtube comme ceci : `<div data-video-btn="YOUTUBE_ID"></div>`.
+
---
+++ src/VideoPlayer.js
@@ -0,0 +1,95 @@
+'format es6';
+'use strict';
+
+import $ from 'jquery';
+
+let videoOverlay;
+let player;
+let close;
+let shareURL;
+
+const CLOSE_ANIMATION_TIME = 600;
+const ACTIVE_CLASS = 'opened';
+
+const TARGET_ELEMENT_DATA = 'video-btn';
+const TARGET_ELEMENT_SELECTOR = `[data-${TARGET_ELEMENT_DATA}]`;
+
+const FB_APP_ID = '';
+const EVENT_NAMESPACE = 'lagrange';
+
+function closeVideoPlayer(e) {
+	e.preventDefault();
+
+	videoOverlay.removeClass(ACTIVE_CLASS);
+	setTimeout(() => {
+		player.empty();
+	}, CLOSE_ANIMATION_TIME);
+}
+
+function openVideoPlayer(e) {
+	e.preventDefault();
+
+	const clicked = $(e.currentTarget);
+	const html = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${clicked.data(TARGET_ELEMENT_DATA)}?autoplay=1&rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen></iframe>`;
+
+	shareURL = `http://youtu.be/${clicked.data(TARGET_ELEMENT_DATA)}`;
+	player.empty().append(html);
+	videoOverlay.addClass(ACTIVE_CLASS);
+}
+
+export default {
+	init() {
+		const playerBtns = $(TARGET_ELEMENT_SELECTOR);
+		videoOverlay = $(`
+			<div class="video-overlay">
+				<div class="close">&times;</div>
+				<div class="share">
+					<div class="facebook share-button" data-facebook data-video-share>
+						<i class="facebook icon-facebook"></i>
+					</div>
+					<div class="twitter share-button" data-twitter data-video-share>
+						<i class="twitter icon-twitter"></i>
+					</div>
+					<div class="linkedin share-button" data-linkedin data-video-share>
+						<i class="linkedin icon-linkedin"></i>
+					</div>
+				</div>
+				<div class="player"></div>
+			</div>
+		`);
+		player = videoOverlay.find('.player');
+		close = videoOverlay.find('.close');
+		
+		if (playerBtns.length === 0) return;
+
+		$('body').append(videoOverlay);
+
+		const facebookBtn = $('[data-facebook]', videoOverlay);
+		const twitterBtn = $('[data-twitter]', videoOverlay);
+		const linkedinBtn = $('[data-linkedin]', videoOverlay);
+
+		facebookBtn.on(`click.${EVENT_NAMESPACE}.video_player`, (e) => {
+			e.preventDefault();
+		
+			const url = `https://www.facebook.com/dialog/share?app_id=${FB_APP_ID}&display=popup&href=${shareURL}`;
+			window.open(url, 'Partager', 'width=520,height=570');
+		});
+
+		twitterBtn.on(`click.${EVENT_NAMESPACE}.video_player`, (e) => {
+			e.preventDefault();
+
+			const url = `https://twitter.com/intent/tweet/?url=${shareURL}`;
+			window.open(url, 'Partager', 'width=560,height=250');
+		});
+
+		linkedinBtn.on(`click.${EVENT_NAMESPACE}.video_player`, (e) => {
+			e.preventDefault();
+
+			const url = `https://www.linkedin.com/cws/share?url=${shareURL}`;
+			window.open(url, 'Partager', 'width=520,height=570');
+		});
+
+		close.on(`click.${EVENT_NAMESPACE}.video_player`, closeVideoPlayer);
+		playerBtns.on(`click.${EVENT_NAMESPACE}.video_player`, openVideoPlayer);
+	},
+};
